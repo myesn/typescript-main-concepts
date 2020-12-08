667f170827c6494f622c6ccceea285ea41261ece
myesn <myesn@foxmail.com>
2020-12-08 23:34:57 +0800

    ♻️ refactor code

--- hello.ts
+++ hello.ts
@@ -1,10 +1,9 @@
-function say(person:string){
-  if(typeof person !== 'string')
-    throw new Error("person is not a string");
+function say(person: string) {
+  if (typeof person !== 'string') throw new Error('person is not a string');
 
   return `Hello, ${person}`;
 }
 
 const user = 'myesn';
-//const user = 1; // 编译不通过，IDE也会有错误的波浪线提示
+// const user = 1; // compile error
 console.log(say(user));
